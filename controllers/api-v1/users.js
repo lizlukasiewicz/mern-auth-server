1db7e6c8a330035df81a80727f9475c500380f60
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-07-08 12:05:28 -0400

    codealong 7/7

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -49,6 +49,38 @@ router.post ('/register', async (req, res) => {
 })
 
 // POST /user/login --- validate login credentials
+router.post('/login', async (req, res) => {
+    try {
+        //try to find the user in the database ((with the emailz))req.body.emai;
+        const findUser = await db.User.findOne({
+            email: req.body.email
+        })
+        const validationFailedMsg = 'Incorrect credentials 🔫  do it again, do it better'
+
+        //if the user is found -- return immediately to register
+        if(!findUser) return res.status(400).json({ msg: validationFailedMsg })
+
+        //check the users password from the db against what is in the req.body
+        const matchPassword = await bcrypt.compare(req.body.password, findUser.password)
+
+        //if the password doesnt match, -- return immediately
+        if(!matchPassword) return res.status(400).json({ msg: validationFailedMsg })
+
+        // create the jwt payload
+        const payload = {
+            name: findUser.name,
+            email: findUser.email,
+            id: findUser.id
+        }
+
+        //sign the jwt and send it back
+        const token = await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60*60 })
+        res.json({ token })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'internal server error' })
+    }
+})
 
 // GET /auth-locked -- middleware that checks if userware is allowed to be here - will redirect if a bad jwt is found
 
